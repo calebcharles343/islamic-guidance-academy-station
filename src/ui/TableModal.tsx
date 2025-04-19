d94a2b991f3750fff90b4d2f14ea26c796880e29
Charles Caleb <charlesofficial9@gmail.com>
2025-04-19 07:38:17 +0100

    update

--- src/ui/TableModal.tsx
+++ src/ui/TableModal.tsx
@@ -8,8 +8,8 @@ interface ModalProps {
 
 const TableModal: React.FC<ModalProps> = ({ children, onClose }) => {
   return ReactDOM.createPortal(
-    <div className="  fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
-      <div className=" w-screen md:w-max bg-white bg-opacity-5 relative rounded shadow-lg  overflow-y-scroll ">
+    <div className="  fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm pt-16">
+      <div className=" w-screen md:w-max bg-white bg-opacity-5 relative rounded shadow-lg  overflow-y-scroll  ">
         <button
           className="text-xs md:text-sm bg-white absolute top-2 right-2 m-2 text-red-500 border border-red-500 py-1 px-4 rounded-lg "
           onClick={onClose}
