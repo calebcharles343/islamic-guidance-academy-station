d2324959bfae3121a593302142d593848fd39dcc
Charles Caleb <charlesofficial9@gmail.com>
2025-05-18 12:18:02 +0100

    update

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,6 +1,10 @@
 export const baseUrl = () => {
-  // const url =
-  //   "https://islamic-guidance-academy.onrender.com/api/v1/islamic-guidance-academy";
-  const url = "http://127.0.0.1:4001/api/v1/islamic-guidance-academy";
-  return url;
+  return import.meta.env.VITE_API_BASE_URL;
 };
+
+// export const baseUrl = () => {
+//   const url =
+//     "https://islamic-guidance-academy.onrender.com/api/v1/islamic-guidance-academy";
+//   // const url = "http://127.0.0.1:4001/api/v1/islamic-guidance-academy";
+//   return url;
+// };
